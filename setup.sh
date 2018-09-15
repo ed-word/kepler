6fec88a88e1f9dae6a95b79948d2ac1dff2c9c4b
edward <edward@EDward.com>
2018-09-15 11:38:19 +0530

    Remove grive from setup.sh

--- setup.sh
+++ setup.sh
@@ -9,17 +9,17 @@ sudo apt upgrade
 
 sudo apt install zip unzip
 
-sudo apt-get install git cmake build-essential libgcrypt11-dev libyajl-dev \
-    libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev \
-    debhelper zlib1g-dev dpkg-dev pkg-config
+# sudo apt-get install git cmake build-essential libgcrypt11-dev libyajl-dev \
+#     libboost-all-dev libcurl4-openssl-dev libexpat1-dev libcppunit-dev binutils-dev \
+#     debhelper zlib1g-dev dpkg-dev pkg-config
 
-git clone https://github.com/vitalif/grive2.git
+# git clone https://github.com/vitalif/grive2.git
 
-cd grive2
-mkdir build
-cd build
-cmake ..
-make -j4
-sudo make install
+# cd grive2
+# mkdir build
+# cd build
+# cmake ..
+# make -j4
+# sudo make install
 
-grive -a -s zip
+# grive -a -s zip
